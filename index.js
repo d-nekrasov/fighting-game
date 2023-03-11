5cd2cfd63908aacb9e4a6e9b5e996ec7ba0542d6
Denys Nekrasov <16300701+d-nekrasov@users.noreply.github.com>
2023-03-11 13:33:58 +0300

    Движение игроков и прослушка событий

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const CANVAS = document.querySelector('canvas');
 const c = CANVAS.getContext('2d');
-const GRAVITY = 0.2;
+const GRAVITY = 0.7;
+const KEYS = {
+	a: {
+		pressed: false
+	},
+	d: {
+		pressed: false
+	},
+	w: {
+		pressed: false
+	},
+	ArrowRight: {
+		pressed: false
+	},
+	ArrowLeft: {
+		pressed: false
+	},
+	ArrowUp: {
+		pressed: false
+	}
+}
 
 CANVAS.width = 1024;
 CANVAS.height = 576;
@@ -8,11 +28,12 @@ CANVAS.height = 576;
 c.fillRect(0, 0, CANVAS.width, CANVAS.height)
 
 
-class Sprite {
+class Player {
 	constructor({position, velocity}) {
 		this.position = position;
 		this.velocity  = velocity;
 		this.height = 150;
+		this.lastKey
 	}
 
 	draw() {
@@ -22,7 +43,7 @@ class Sprite {
 
 	update() {
 		this.draw();
-
+		this.position.x += this.velocity.x
 		this.position.y += this.velocity.y
 
 		if(this.position.y + this.height + this.velocity.y >= CANVAS.height) {
@@ -34,7 +55,7 @@ class Sprite {
 	}
 }
 
-const player = new Sprite({
+const player1 = new Player({
 	position: {
 		x: 0, y: 0,
 	},
@@ -44,7 +65,7 @@ const player = new Sprite({
 })
 
 
-const enemy = new Sprite({
+const player2 = new Player({
 	position: {
 		x: 400, y: 0,
 	},
@@ -57,10 +78,86 @@ const enemy = new Sprite({
 
 function animate() {
 	window.requestAnimationFrame(animate)
-	c.fillStyle = '#000'
+	c.fillStyle = '#000000'
 	c.fillRect(0,0, CANVAS.width, CANVAS.height)
-	player.update();
-	enemy.update()
+	c.fillStyle = "#ffffff";
+	c.font = '24px Sans-Serif'
+	c.fillText('P1', player1.position.x, player1.position.y)
+	c.fillText('P2', player2.position.x, player2.position.y)
+	player1.update();
+	player2.update()
+
+	//Player 1 movement
+	player1.velocity.x = 0
+	if(KEYS.a.pressed && player1.lastKey === 'a'){
+		player1.velocity.x = -5;
+	}else if(KEYS.d.pressed &&  player1.lastKey === 'd') {
+		player1.velocity.x = 5;
+	}
+
+	//Player 2 movement
+	player2.velocity.x = 0
+	if(KEYS.ArrowLeft.pressed && player2.lastKey === 'ArrowLeft'){
+		player2.velocity.x = -5;
+	}else if(KEYS.ArrowRight.pressed && player2.lastKey === 'ArrowRight') {
+		player2.velocity.x = 5;
+	}
+
 }
 
 animate()
+
+
+window.addEventListener('keydown', (event)=>{
+	console.log(event.key)
+	switch (event.key) {
+		//Player 1
+		case 'd':
+			KEYS.d.pressed = true
+			player1.lastKey = 'd'
+			break
+		case 'a':
+			KEYS.a.pressed = true
+			player1.lastKey = 'a'
+			break
+		case 'w':
+			player1.velocity.y = -20
+			break
+		//Player 2
+
+		case 'ArrowRight':
+			KEYS.ArrowRight.pressed = true
+			player2.lastKey = 'ArrowRight';
+			break
+		case 'ArrowLeft':
+			KEYS.ArrowLeft.pressed = true
+			player2.lastKey = 'ArrowLeft';
+			break
+		case 'ArrowUp':
+			player2.velocity.y = -20
+			break
+	}
+})
+
+window.addEventListener('keyup', (event)=>{
+	switch (event.key) {
+		//Player 1
+		case 'd':
+			KEYS.d.pressed = false
+			break
+		case 'a':
+			KEYS.a.pressed = false
+			break
+
+		//Player 2
+
+		case 'ArrowRight':
+			KEYS.ArrowRight.pressed = false
+			break
+		case 'ArrowLeft':
+			KEYS.ArrowLeft.pressed = false
+			break
+	}
+
+
+})
